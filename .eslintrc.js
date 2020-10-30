0830296bb6465d8e8a6af5f52d5939609446111b
Charly <charly@clic84.fr>
2020-10-30 09:59:58 +0100

    🔧 code quality tools

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  env: {
+    jest: true,
+    browser: true,
+    es6: true,
+  },
+  parser: 'babel-eslint',
+  extends: ['airbnb', 'prettier', 'prettier/react'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+      modules: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'arrow-parens': [2, 'always'],
+    'no-underscore-dangle': ['error', { allow: ['_id'] }],
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [1, { extensions: ['.js'] }],
+    'react/forbid-prop-types': [0, { forbid: ['any'] }],
+    'react/prop-types': 1,
+    'no-console': ['error', { allow: ['warn', 'error'] }],
+    'no-extra-semi': ['error'],
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        moduleDirectory: ['node_modules', 'src/'],
+      },
+    },
+  },
+}
