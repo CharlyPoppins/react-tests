fab8f2c057d1b05fc637ebd9ed39d0f4d05c5212
Charly <charly@clic84.fr>
2020-12-11 12:20:12 +0100

    🐛 fix urlBase64ToUint8Array

--- src/components/Notifications.js
+++ src/components/Notifications.js
@@ -3,21 +3,9 @@ import Button from 'react-bootstrap/Button'
 import { toast } from 'react-toastify'
 import PropTypes from 'prop-types'
 
+import { urlBase64ToUint8Array } from '../helpers/webPush'
+
 const notificationsEnabled = Notification.permission === 'granted'
-
-// Copied from the web-push documentation
-const urlBase64ToUint8Array = (base64String) => {
-  const padding = '='.repeat((4 - (base64String.length % 4)) % 4)
-  const base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/')
-
-  const rawData = window.atob(base64)
-  const outputArray = new Uint8Array(rawData.length)
-
-  for (let i = 0; i < rawData.length; i + 1) {
-    outputArray[i] = rawData.charCodeAt(i)
-  }
-  return outputArray
-}
 
 const Notifications = ({ currentServiceWorker }) => {
   return (
