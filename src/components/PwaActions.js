9ca2d65c2fdcd920b9601fae5ec34e21c2996c19
Charly <charly@clic84.fr>
2020-12-11 21:04:32 +0100

    ✨avoid error in PWA

--- src/components/PwaActions.js
+++ src/components/PwaActions.js
@@ -15,15 +15,22 @@ const PwaActions = () => {
             className="btn-block btn-secondary"
             onClick={() => {
               if ('serviceWorker' in navigator) {
-                console.log('Sending message SKIP_WAITING to SW')
-                navigator.serviceWorker.controller.postMessage({
-                  type: 'SKIP_WAITING',
-                })
+                if (navigator.serviceWorker.controller) {
+                  console.log('Sending message SKIP_WAITING to SW')
+                  navigator.serviceWorker.controller.postMessage({
+                    type: 'SKIP_WAITING',
+                  })
+                } else {
+                  console.warn('Service workers controller does not exist.')
+                }
               } else {
                 console.warn('Service workers are not supported.')
               }
             }}
-            disabled={!('serviceWorker' in navigator)}
+            disabled={
+              !('serviceWorker' in navigator) ||
+              !navigator.serviceWorker.controller
+            }
           >
             Update PWA
           </Button>
@@ -35,14 +42,22 @@ const PwaActions = () => {
             className="btn-block btn-secondary"
             onClick={() => {
               if ('serviceWorker' in navigator) {
-                console.log('Sending message MESSAGE_IDENTIFIER to SW')
-                navigator.serviceWorker.controller.postMessage({
-                  type: 'MESSAGE_IDENTIFIER',
-                })
+                if (navigator.serviceWorker.controller) {
+                  console.log('Sending message MESSAGE_IDENTIFIER to SW')
+                  navigator.serviceWorker.controller.postMessage({
+                    type: 'MESSAGE_IDENTIFIER',
+                  })
+                } else {
+                  console.warn('Service workers controller does not exist.')
+                }
               } else {
                 console.warn('Service workers are not supported.')
               }
             }}
+            disabled={
+              !('serviceWorker' in navigator) ||
+              !navigator.serviceWorker.controller
+            }
           >
             Post message to SW
           </Button>
